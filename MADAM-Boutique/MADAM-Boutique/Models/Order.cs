a30d760ae0b2534d9346dc4bb77f1b0165ed7af4
truongmaixuan <56288717+truongmaixuan@users.noreply.github.com>
2021-01-26 22:33:25 +0700

    Update Models
    Update Models: Order, OrderDetail, CartItem, Customer

---
+++ MADAM-Boutique/MADAM-Boutique/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MADAM_Boutique.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public System.DateTime OrderDate { get; set; }
+
+        [ScaffoldColumn(false)]
+        public decimal Total { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string PaymentTransactionId { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool HasBeenShipped { get; set; }
+
+        public Customer Customer { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ MADAM-Boutique/MADAM-Boutique/Models/OrderDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MADAM_Boutique.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double? UnitPrice { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
